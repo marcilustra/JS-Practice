68d33b0d594be893205e94764b4532851d213d42
marcilustra <theknifebox69@gmail.com>
2023-07-03 23:46:19 +0300

    Task complete

--- 07. Sunday Jan1st/script.js
+++ 07. Sunday Jan1st/script.js
@@ -1,11 +1,19 @@
 function isItSunday() {
-    let today = new Date();
-    today.setFullYear(new Date().getFullYear() - 9);
+    let today = new Date("January 1, 2014");
+    // today.setFullYear(new Date().getFullYear());
+    let lastDay = new Date("December 31, 2050");
     
-    if(today.getDate() == 1 && today.getMonth() == 0){
-        console.log("1st January 183 days from now\n");
+    var loop = new Date(today);
+    while(loop <= lastDay) {
+        if(loop.getDate() == 1 && loop.getMonth() == 0 && loop.getDay() == 0){
+            console.log("1st January is a Sunday for date:\n" + loop + "\n");
+        }
+        // console.log(loop.getDate() + "\n");
+        var newDate = loop.setDate(loop.getDate() + 1);
+        loop = new Date(newDate);
     }
-    return today;
+    return today.getFullYear();
 }
 
-console.log(isItSunday());
+// console.log(isItSunday());
+isItSunday();
