14a06ee526cf10ad4bb753ab589b5fd765209a83
marcilustra <theknifebox69@gmail.com>
2023-07-03 23:46:39 +0300

    Loop thru years,Comments included

--- 07. Sunday Jan1st/script.js
+++ 07. Sunday Jan1st/script.js
@@ -1,19 +1,22 @@
 function isItSunday() {
+    //First we create 2 variables storing the dates 1st Jan 2014
+    //And 31st Dec 2050
     let today = new Date("January 1, 2014");
-    // today.setFullYear(new Date().getFullYear());
-    let lastDay = new Date("December 31, 2050");
+    let lastDay = new Date("January 1, 2050");
     
+    //loop is meant to hold the value of today as we count thru to 2050 Dec 31
     var loop = new Date(today);
     while(loop <= lastDay) {
+        //Check if Jan 1st of a Year is on a Sunday(0)
         if(loop.getDate() == 1 && loop.getMonth() == 0 && loop.getDay() == 0){
             console.log("1st January is a Sunday for date:\n" + loop + "\n");
         }
-        // console.log(loop.getDate() + "\n");
-        var newDate = loop.setDate(loop.getDate() + 1);
+        //Add to the date jumping to next year since we are only checking Januaries on 1st.
+        var newDate = loop.setFullYear(loop.getFullYear() + 1);
         loop = new Date(newDate);
     }
     return today.getFullYear();
 }
 
-// console.log(isItSunday());
+
 isItSunday();
